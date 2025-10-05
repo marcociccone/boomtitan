83ddf4cf0ce2a95fa567e81dd52d9347efbe9424
marcociccone <marco.ciccone@me.com>
2025-10-05 03:55:07 +0200

    fix resume training with vocab size not divisible by world size

--- torchtitan/components/checkpoint.py
+++ torchtitan/components/checkpoint.py
@@ -76,7 +76,9 @@ class ModelWrapper(Stateful):
         return state_dict
 
     def state_dict(self) -> dict[str, Any]:
-        return self.cache_state_dict
+        # Fix from https://github.com/pytorch/torchtitan/pull/1702
+        # https://github.com/pytorch/torchtitan/issues/1136
+        return self._get_state_dict()
 
     def load_state_dict(self, state_dict: dict[str, Any]) -> None:
         func = functools.partial(
